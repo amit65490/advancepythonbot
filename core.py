213cf679ab7ad480e88073597051cc9865c1ebfd
amit65490 <68516137+amit65490@users.noreply.github.com>
2022-09-29 20:07:05 +0530

    Create core.py

---
+++ core.py
@@ -0,0 +1,8 @@
+#Bot Token From @BotFather
+bot_token = "5661682356:AAERxHY26HOQznRkIhH2qOkbSzVn7MQ8QQ0"
+
+#Admins Telegram ID
+admins = [868578453]
+
+#MongoDb Url For Databse
+mongo_url = "mongodb+srv://royalayush:royalayush@cluster0.p77fj.mongodb.net/NodeJSBot?retryWrites=true&w=majority"
